55c5956d6a08cb88f0cb895a138e3b68176abc32
Mohamed Refaat <muhamedmahmmod@gmail.com>
2022-02-20 23:49:15 +0200

    add dagger-hilt implementation

---
+++ app/src/main/java/com/refaat/themoviesdb/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.refaat.themoviesdb
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+
+
+
+}
---
+++ app/src/main/java/com/refaat/themoviesdb/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.refaat.themoviesdb.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+
+
+}
