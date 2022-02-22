8f5487b3107160792aaeb25f45b853599728ec79
Mohamed Refaat <muhamedmahmmod@gmail.com>
2022-02-22 21:07:30 +0200

    add genre mapping for search movies result

--- app/src/main/java/com/refaat/themoviesdb/domain/useCases/GetMoviesOfSearchQueryUseCase.kt
+++ app/src/main/java/com/refaat/themoviesdb/domain/useCases/GetMoviesOfSearchQueryUseCase.kt
@@ -1,14 +1,27 @@
 package com.refaat.themoviesdb.domain.useCases
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
 import androidx.paging.PagingData
+import androidx.paging.map
 import com.refaat.themoviesdb.domain.model.Movie
 import com.refaat.themoviesdb.domain.repository.TheMovieDbRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class GetMoviesOfSearchQueryUseCase (private val repository: TheMovieDbRepository) {
     operator fun invoke(searchQuery:String): LiveData<PagingData<Movie>> {
-        return repository.getMoviesOfSearchQuery(searchQuery)
+        return repository.getMoviesOfSearchQuery(searchQuery).map { pagingData ->
+            pagingData.map { movie ->
+                val genreListString: ArrayList<String> = arrayListOf()
+                movie.genreIds.forEach {
+                    val name = repository.getGenreNameById(it)
+                    genreListString.add(name)
+                }
+                movie.genreString = genreListString.joinToString()
+                movie
+            }
+        }
     }
 
 }
